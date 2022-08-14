d744dc0fa71c05effd9dcc647f8c355dd32036e2
Tomasz <snakerrek@gmail.com>
2022-08-14 12:37:19 +0200

    Wrapped bookService methods in try-catch blocks and added successful statusCode.

--- services/bookService.js
+++ services/bookService.js
@@ -65,41 +65,56 @@ const filterBooksForISBN = (isbn, allBooks) => {
 };
 
 const searchByAuthor = async (req, res) => {
-  const author = req.params.author;
-  const allBooks = await databaseService.getAllBooks();
+  try {
+    const author = req.params.author;
+    const allBooks = await databaseService.getAllBooks();
 
-  res.json(filterBooksForAuthor(author, allBooks));
+    res.status(200).json(filterBooksForAuthor(author, allBooks));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const searchByTitle = async (req, res) => {
-  const title = req.params.title;
-  const allBooks = await databaseService.getAllBooks();
+  try {
+    const title = req.params.title;
+    const allBooks = await databaseService.getAllBooks();
 
-  res.json(filterBooksForTitle(title, allBooks));
+    res.status(200).json(filterBooksForTitle(title, allBooks));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const searchByISBN = async (req, res) => {
-  const isbn = req.params.isbn;
-  const allBooks = await databaseService.getAllBooks();
-  console.log(filterBooksForISBN(isbn, allBooks));
-  res.json(filterBooksForISBN(isbn, allBooks));
+  try {
+    const isbn = req.params.isbn;
+    const allBooks = await databaseService.getAllBooks();
+    console.log(filterBooksForISBN(isbn, allBooks));
+    res.status(200).json(filterBooksForISBN(isbn, allBooks));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const searchBooks = async (req, res) => {
-  const searchParams = req.params.searchParams;
-  const allBooks = await databaseService.getAllBooks();
+  try {
+    const searchParams = req.params.searchParams;
+    const allBooks = await databaseService.getAllBooks();
 
-  const searchByISBNResult = filterBooksForISBN(searchParams, allBooks);
-  const searchByTitleResult = filterBooksForTitle(searchParams, allBooks);
-  const searchByAuthorResult = filterBooksForAuthor(searchParams, allBooks);
+    const searchByISBNResult = filterBooksForISBN(searchParams, allBooks);
+    const searchByTitleResult = filterBooksForTitle(searchParams, allBooks);
+    const searchByAuthorResult = filterBooksForAuthor(searchParams, allBooks);
 
-  const foundBooks = [
-    ...searchByISBNResult,
-    ...searchByTitleResult,
-    ...searchByAuthorResult,
-  ];
-
-  res.json(removeDuplicatedBooks(foundBooks));
+    const foundBooks = [
+      ...searchByISBNResult,
+      ...searchByTitleResult,
+      ...searchByAuthorResult,
+    ];
+    res.status(200).json(removeDuplicatedBooks(foundBooks));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 exports.getAllBooks = getAllBooks;
