136ffc808a242dd9219e300d5780809f5288056f
Tomasz <snakerrek@gmail.com>
2022-08-20 18:49:07 +0200

    Now search requests are returning list of book only with basic crucial information

--- services/databaseService.js
+++ services/databaseService.js
@@ -1,7 +1,10 @@
 const Book = require("../models/Book");
 
 const getAllBooks = async () => {
-  return await Book.find({});
+  return await Book.find(
+    {},
+    { description: 0, isbn: 0, pageCount: 0, categories: 0 }
+  );
 };
 
 exports.getAllBooks = getAllBooks;
